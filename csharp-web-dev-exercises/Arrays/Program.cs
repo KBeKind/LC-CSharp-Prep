462aa16cf558020c87fd435871a19db7d232f4a9
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-24 10:26:39 -0600

    commit

--- csharp-web-dev-exercises/Arrays/Program.cs
+++ csharp-web-dev-exercises/Arrays/Program.cs
@@ -4,3 +4,14 @@
 
 Console.WriteLine("Hello, Arrays!");
 
+int[] intArray = new int[] {1, 1, 2, 3, 5, 8 };
+
+
+foreach( int value in intArray)
+{
+    if (value % 2 != 0)
+    {
+		Console.WriteLine(value);
+	}
+
+}
