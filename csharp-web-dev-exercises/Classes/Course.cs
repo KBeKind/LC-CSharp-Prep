d060bc99f7d39da8cfd9cd7f01726d9aba9d2834
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-25 09:03:08 -0600

    commit

---
+++ csharp-web-dev-exercises/Classes/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+	public class Course
+	{
+		public string CourseTitle { get; set; }
+		public Teacher Teacher { get; set; }
+		public List<Student> StudentsList { get; set; } = new List<Student>();
+
+	}
+}
--- csharp-web-dev-exercises/Classes/Program.cs
+++ csharp-web-dev-exercises/Classes/Program.cs
@@ -1,1 +1,28 @@
 ﻿// TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+
+using Classes;
+
+Student aStudent = new Student("Dudeman", 1, 4.0);
+
+Console.WriteLine(aStudent.Name);
+Console.WriteLine(aStudent.StudentId);
+Console.WriteLine(aStudent.NumberOfCredits);
+Console.WriteLine(aStudent.Gpa);
+
+
+Teacher aTeacher = new Teacher();
+aTeacher.FirstName = "Billy";
+aTeacher.LastName = "Bob";
+aTeacher.YearsTeaching = 10;
+aTeacher.Subject = "Programming";
+
+Course aCourse = new Course();
+
+aCourse.CourseTitle = "Program Awesomely";
+aCourse.Teacher = aTeacher;
+
+aCourse.StudentsList.Add(aStudent);
+
+
+Console.WriteLine($"Course: {aCourse.CourseTitle} is taught by {aCourse.Teacher.FirstName} {aCourse.Teacher.LastName} and has {aStudent.Name} enrolled.");
