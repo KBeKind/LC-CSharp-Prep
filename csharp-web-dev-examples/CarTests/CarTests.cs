da73d2ff94383bbf2ec1431131d55f1931d41c88
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-27 08:50:55 -0600

    commit

--- csharp-web-dev-examples/CarTests/CarTests.cs
+++ csharp-web-dev-examples/CarTests/CarTests.cs
@@ -1,11 +1,42 @@
 ﻿namespace CarTests;
 
+using Car;
+
 [TestClass]
 public class CarTests
 {
+    Car test;
+
+    [TestInitialize]
+    public void CreateCarObject()
+    {
+		test = new Car("Brand", "Model", 10, 50);
+	}
+
+
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
+    [TestMethod]
+    public void EmptyTest()
+    {
+        Assert.AreEqual(10, 10, .001);
+    }
+
+
     //TODO: constructor sets gasTankLevel properly
+    [TestMethod]
+    public void TestInitialGasTank()
+    {
+        Assert.AreEqual(10, test.GasTankLevel, .001);
+    }
+
+
     //TODO: gasTankLevel is accurate after driving within tank range
+
+
+
+
+
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
 }
