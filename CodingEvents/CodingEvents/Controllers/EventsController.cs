b36e64b34e96ad0113cb252433ae40a7bbe46916
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-03-14 17:40:37 -0500

    commit

---
+++ CodingEvents/CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+	[Route("Events")]
+	public class EventsController : Controller
+	{
+		static private List<string> Events = new List<string>();
+		
+		
+
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+
+
+
+			ViewBag.events = Events;
+
+			return View();
+		}
+
+
+		[HttpGet]
+		[Route("Add")]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		[Route("Add")]
+		public IActionResult NewEvent(string name)
+		{
+			Events.Add(name);
+			return Redirect("/Events");
+		}
+	
+		
+
+
+
+
+	}
+}
