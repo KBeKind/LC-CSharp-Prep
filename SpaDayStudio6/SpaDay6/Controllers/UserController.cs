0cee896c98a9ba7a996c4011cc02c7601b6541a7
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-03-20 08:52:05 -0500

    commit

--- SpaDayStudio6/SpaDay6/Controllers/UserController.cs
+++ SpaDayStudio6/SpaDay6/Controllers/UserController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay6.Models;
 
 namespace SpaDay6.Controllers
 {
+	[Route("/user")]
     public class UserController : Controller
     {
-        public IActionResult Index()
+
+        public IActionResult Index() { return View(); }
+       
+
+        [HttpGet]
+        [Route("add")]
+        public IActionResult Add()
         {
             return View();
         }
-    }
+
+
+        [HttpPost]
+        [Route("add")]
+		public IActionResult SubmitAddUserForm(User newUser, string verify)
+		{
+			// add form submission handling code here
+
+            if(newUser.Password.Equals(verify))
+            {
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                newUser.DateCreated = DateTime.Now;
+
+                ViewBag.dateCreated = newUser.DateCreated.ToString();
+
+
+                return View("Index");
+            }
+
+
+            ViewBag.error = "Passwords must match";
+            ViewBag.username = newUser.Username;
+            ViewBag.email = newUser.Email;
+
+            return View("Add");
+
+
+		}
+
+	}
+
 }
