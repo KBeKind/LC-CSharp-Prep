6f412ea28a612f21dd86fa80d5651be62c12e612
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-03-21 16:58:45 -0500

    commit

--- CodingEvents/CodingEvents/ViewModels/AddEventViewModel.cs
+++ CodingEvents/CodingEvents/ViewModels/AddEventViewModel.cs
@@ -1,10 +1,19 @@
-﻿namespace CodingEvents.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels
 {
 	public class AddEventViewModel
 	{
+		[Required(ErrorMessage = "Event Name is required")]
+		[StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters long")]
+		public string? Name { get; set; }
 
-		public string? Name { get; set; } 
+		[Required(ErrorMessage = "Description is required")]
+		[StringLength(60, MinimumLength = 3, ErrorMessage = "Description must be between 3 and 60 characters long")]
 		public string? Description { get; set; }
+
+		[EmailAddress]
+		public string? ContactEmail { get; set; }
 
 	}
 }
